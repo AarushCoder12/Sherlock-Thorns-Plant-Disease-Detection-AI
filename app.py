831bd5c1037d7e27da134b1e122fdb4704c76800
AarushCoder12 <aarushr.konyala@gmail.com>
2026-01-05 18:03:59 -0800

    Refactor plant disease detection model and classes
    Updated the model loading and class names for plant disease detection. Added new plants and modified disease handling.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
-from tensorflow import keras
+import tensorflow as tf
 from PIL import Image
 import numpy as np
+import cv2
 
 # Cache the model loading to prevent reloading on every interaction
 @st.cache_resource
 def load_model():
-    model = keras.models.load_model("plant_ai.keras")
+    model = tf.keras.models.load_model("PlantAImodel(94% train to 81% valid).keras")
     return model
 
 # Load the pre-trained model using the cached function
@@ -14,7 +15,42 @@ model = load_model()
 
 #Website title and description
 st.title("Sherlock Thorns: Plant Disease Detection AI")
-st.write("Upload an image of a plant leaf to detect diseases. The plant must be one of the following: Apple, Cherry, Corn, Grape, Peach, Pepper, Potato, Strawberry, Tomato.")
+st.write("Upload an image of a plant leaf to detect diseases. The plant must be one of the following: Apple, Blueberry, Cherry, Corn, Grape, Orange, Peach, Pepper, Potato, Raspberry, Soybean, Squash, Strawberry, Tomato.")
+
+
+##File uploader
+#uploaded_file = st.file_uploader("Upload an image", type=["jpg","png","jpeg"])
+
+#if uploaded_file is not None:
+#    # Open image
+#    image = Image.open(uploaded_file).convert("RGB")
+#    img = np.array(image)
+
+ #    hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+ #   # Define green color range (leaf)
+ #   lower_green = np.array([15, 20, 20])
+ #   upper_green = np.array([100, 255, 255])
+
+#    # Create mask for leaf
+#    mask = cv2.inRange(hsv, lower_green, upper_green)
+
+#    # Create black background
+#    black_bg = np.zeros_like(img)
+
+#    # Keep only leaf pixels
+#    segmented = cv2.bitwise_and(img, img, mask=mask)
+
+#    # Resize & normalize
+#   target_size = (224, 224)
+#    img_array = cv2.resize(segmented, target_size) / 255.0
+ #   img_array = np.expand_dims(img_array, axis=0)
+
+    # Predict
+ #   predictions = model.predict(img_array)
+#    index = np.argmax(predictions)
+ #   confidence = predictions[0][index] * 100
+
 
 #File uploader
 uploaded_file = st.file_uploader("Upload an image", type=["jpg","png","jpeg"])
@@ -32,54 +68,56 @@ if uploaded_file is not None:
     predictions = model.predict(img_array)
     index = np.argmax(predictions)
     confidence = predictions[0][index]*100
+
     #Class names
     class_names = [
-             "Apple_AppleScab",
-             "Apple_BlackRot",
-             "Apple_AppleRust",
-             "Apple_Healthy",
-             "Blueberry_Healthy",
-             "Cherry_PowderyMildew",
-             "Cherry_Healthy",  
-             "Corn_CercosporaLeafSpot",
-             "Corn_CommonRust",
-             "Corn_NorthernLeafBlight",
-             "Corn_Healthy",
-             "Grape_BlackRot",
-             "Grape_Esca",
-             "Grape_LeafBlight",
-             "Grape_Healthy",
-             "Orange_Haunglongbing",
-             "Peach_BacterialSpot",
-             "Peach_Healthy",
-             "Pepper_BacterialSpot",
-             "Pepper_Healthy",
-             "Potato_EarlyBlight",
-             "Potato_LateBlight",
-             "Potato_Healthy",
-             "Raspberry_Healthy",
-             "Soybean_Healthy",
-             "Squash_PowderyMildew",
-             "Strawberry_LeafScorch",
-             "Strawberry_Healthy",
-             "Tomato_BacterialSpot",
-             "Tomato_EarlyBlight",
-             "Tomato_LateBlight",
-             "Tomato_Leaf_Mold",
-             "Tomato_Septoria_leaf_spot",
-             "Tomato_SpiderMites",
-             "Tomato_TargetSpot",
-             "Tomato_YellowLeafCurlVirus",
-             "Tomato__MosaicVirus",
-             "Tomato_Healthy",
-             
-    ]
-    st.subheader("Prediction")
+    "Apple_AppleScab",
+    "Apple_BlackRot",
+    "Apple_AppleRust",
+    "Apple_Healthy",
+    "Blueberry_Healthy",
+    "Cherry_PowderyMildew",
+    "Cherry_Healthy",  
+    "Corn_CercosporaLeaTfSpot",
+    "Corn_CommonRust",
+    "Corn_NorthernLeafBlight",
+    "Corn_Healthy",
+    "Grape_BlackRot",
+    "Grape_Esca",
+    "Grape_LeafBlight",
+    "Grape_Healthy",
+    "Orange_Haunglongbing",  
+    "Peach_BacterialSpot",
+    "Peach_Healthy",
+    "Pepper_BacterialSpot",
+    "Pepper_Healthy",#
+    "Potato_EarlyBlight",
+    "Potato_LateBlight",
+    "Potato_Healthy",
+    "Raspberry_Healthy",
+    "Soybean_Healthy",
+    "Squash_PowderyMildew",
+    "Strawberry_LeafScorch",
+    "Strawberry_Healthy",
+    "Tomato_BacterialSpot",
+    "Tomato_EarlyBlight",
+    "Tomato_LateBlight",
+    "Tomato_LeafMold",
+    "Tomato_SeptoriaLeafSpot",
+    "Tomato_SpiderMites",
+    "Tomato_TargetSpot",
+    "Tomato_YellowLeafCurlVirus",
+    "Tomato_MosaicVirus",   
+    "Tomato_Healthy",
+]
+
+ 
     if class_names[index].endswith("Healthy"):
-        plant_name=class_names[index].split("_")[0]
+        plant_name = class_names[index].split("_")[0]
         st.success(plant_name+f" plant is healthy: No disease detected.")
     else: 
-        st.write(f"The detected disease is: {class_names[index]}")
+        plant_name = class_names[index].split("_")[0]
+        st.write(f"The detected disease for {plant_name} is {class_names[index].split('_')[1]}")
     st.write(f"I am {confidence:.1f}% Confident my Response is Accurate")
     st.image(image,caption="Uploaded Image", use_column_width=True)
      
@@ -108,7 +146,7 @@ if uploaded_file is not None:
             st.warning("Remove and destroy infected plants. Use disease-free planting material.")
         elif class_names[index].endswith("SpiderMites"):
             st.warning("Use insecticidal soap or neem oil. Increase humidity and water plants regularly.")
-        elif class_names[index].endswith("Septoria_leaf_spot"):
+        elif class_names[index].endswith("Septorialeafspot"):
             st.warning("Apply fungicides containing chlorothalonil or mancozeb. Remove and destroy infected leaves.")
         elif class_names[index].endswith("TargetSpot"):
             st.warning("Apply fungicides containing azoxystrobin or pyraclostrobin. Remove and destroy infected leaves.")
@@ -118,5 +156,5 @@ if uploaded_file is not None:
             st.warning("Apply fungicides containing chlorothalonil or mancozeb. Remove and destroy infected leaves.")
         elif class_names[index].endswith("Haunglongbing"):
             st.warning("Use insecticidal soap or neem oil. Increase humidity and water plants regularly.")
-        else: 
-            st.warning("The disease is not recognized. Consult a plant expert for further advice.")
+        elif class_names[index].endswith("CercosporaLeafSpot"): 
+            st.warning("Apply fungicides containing azoxystrobin or pyraclostrobin. Remove and destroy infected leaves.")
